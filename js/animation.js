7216fd4244b51abdb032fc459332d31bb58f3075
CommandMaker <command.maker83@gmail.com>
2024-01-28 18:24:18 +0100

    feat: homepage animations

---
+++ js/animation.js
@@ -0,0 +1,32 @@
+/**
+ * Function triggered when an element is now visible on screen
+ *
+ * @param {IntersectionObserverEntry[]} entries Elements that are now visible
+ * @param {IntersectionObserver} _ The observer object
+ */
+const onVisible = (entries, _) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animation');
+            entry.target.classList.remove('unvisible');
+        }
+    })
+}
+
+const observer = new IntersectionObserver(onVisible, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1
+});
+
+/** @var {string[]} elementsToAnimate CSS selector of elements to animate */
+const elementsToAnimate = [
+    'section#probleme h2',
+    'section#probleme div.content',
+    'section#solutions h2',
+    'section#solutions .splitted'
+];
+
+elementsToAnimate.forEach(el => {
+    observer.observe(document.querySelector(el));
+});
--- js/app.js
+++ js/app.js
@@ -1,1 +1,2 @@
+import './animation.js';
 import './header.js';
